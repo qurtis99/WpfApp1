352db55e4bc5a8560961537a308d2e8800049ae3
qurtis99 <kokhanevych.dmytro@lll.kpi.ua>
2025-04-28 17:27:20 +0300

    Finish

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace DPGI2
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public static readonly RoutedCommand SaveCommand = new RoutedCommand();
+        public static readonly RoutedCommand OpenCommand = new RoutedCommand();
+        public static readonly RoutedCommand ClearCommand = new RoutedCommand();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Прив'язка команди Save
+            CommandBinding saveBinding = new CommandBinding(SaveCommand, SaveExecute, SaveCanExecute);
+            this.CommandBindings.Add(saveBinding);
+
+            // Прив'язка команди Open
+            CommandBinding openBinding = new CommandBinding(OpenCommand, OpenExecute, OpenCanExecute);
+            this.CommandBindings.Add(openBinding);
+
+            // Прив'язка команди Clear
+            CommandBinding clearBinding = new CommandBinding(ClearCommand, ClearExecute, ClearCanExecute);
+            this.CommandBindings.Add(clearBinding);
+        }
+
+        // Обробник події Loaded для встановлення фокусу на TextBox
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            textBox.Focus();
+        }
+
+        // Обробники для Save
+        private void SaveCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true; // Умову можна змінити за потреби
+        }
+
+        private void SaveExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            // Логіка збереження тексту (наприклад, у файл)
+            MessageBox.Show("Text saved!", "Save");
+        }
+
+        // Обробники для Open
+        private void OpenCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true; // Умову можна змінити за потреби
+        }
+
+        private void OpenExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            // Логіка відкриття файлу
+            MessageBox.Show("Open file!", "Open");
+        }
+
+        // Обробники для Clear
+        private void ClearCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true; // Умову можна змінити за потреби
+        }
+
+        private void ClearExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            textBox.Text = string.Empty; // Очищення текстового поля
         }
     }
 }
